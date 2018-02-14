79ddd9a92fac75d7b807fc0e41e6bfa4ce45c86e
Mii Erik Samyo Haugård <mii.erik@haugard.no>
2018-02-14 19:23:46 +0100

    1 og 2

--- zeta0/zeta0test.c
+++ zeta0/zeta0test.c
@@ -6,6 +6,13 @@ void test_zeta0()
 {
     double results = zeta_function(3);
     printf("Pi is approximately: " + "%d\n", results);
+    double expected_value = 2.858;
+
+    double difference = results - expected_value
+    if (fabs(difference) > 0.001){
+      perror("The function is not returning the expected value");
+      return (-1)
+    }
 }
 
 
