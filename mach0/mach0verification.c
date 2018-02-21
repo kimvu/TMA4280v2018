e532dfa3493f1e6c38dc44f856d9add982c03361
Mii Erik Samyo Haugård <mii.erik@haugard.no>
2018-02-21 19:36:58 +0100

    make for mach

---
+++ mach0/mach0verification.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <assert.h>
+#include "mach0.h"
+#include <math.h>
+#define M_PI 3.14159265358979323846
+
+int verification_zeta0()
+{
+  double errors[24];
+    for (int i = 1; i <= 24; i++){
+      errors[i-1] = (fabs(M_PI - mach_function(pow(2, i))));
+    }
+
+    FILE *f = fopen("verification_results.txt", "w");
+
+    for (int i = 1; i <= 24; i++){
+      printf("%f\n", errors[i-1]);
+      fprintf(f, "%f\n", errors[i-1]);
+    }
+
+}
+
+int main(int argc, char *argv[])
+{
+    int ret = 0;
+    ret |= verification_zeta0();
+    return ret;
+}
